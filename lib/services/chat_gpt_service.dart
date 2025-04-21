d3bcc94595da4b9134ce63daaea518d94932240d
Angel Eugenio Charca Blanco <ablanco837@gmail.com>
2025-04-21 16:32:46 -0400

    configuring new ip to backend

--- lib/services/chat_gpt_service.dart
+++ lib/services/chat_gpt_service.dart
@@ -2,7 +2,7 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 Future<Map<String, dynamic>> sendRequest(String query) async {
-  const String apiUrl = "https://backendcbatalk-production.up.railway.app/api/consulta";
+  const String apiUrl = "http://52.15.107.244/api/consulta";
   final Map<String, String> headers = {"Content-Type": "application/json"};
 
   final Map<String, String> body = {"consulta": query};
